a6bc2627c661abd5ac7336dc35224c1da2d4f727
Jay <liujiekm@126.com>
2016-10-10 18:14:17 +0800

    modify GridEdit

--- src/common/GridEdit.js
+++ src/common/GridEdit.js
@@ -6,16 +6,17 @@ class GridEidt extends Component{
 
         return (
 
-            <div id="49" class="dashboard-edit-menu-container">
-                <div role="button" aria-label="Edit Dashboard" title="Edit Dashboard" class="button menu-toggle-button propagate-keydown-event" tabindex="0">
-                    <div class="button-icon closed-icon bowtie-edit"></div>
-                    <div class="button-icon open-icon bowtie-check"></div>
+            <div className="dashboard-edit-menu-container">
+                <div role="button" aria-label="Edit Dashboard" title="Edit Dashboard" className="button menu-toggle-button propagate-keydown-event" tabindex="0">
+                    <div className="button-icon closed-icon bowtie-edit"></div>
+                    <div className="button-icon open-icon bowtie-check"></div>
                 </div>
-                <div class="edit-menu">
-                    <div role="button" aria-label="Add Widget" title="Add Widget" class="button add-widget-button propagate-keydown-event" tabindex="0">
-                    <div class="button-icon bowtie-math-plus"></div>
+                <div className="edit-menu">
+                    <div role="button" aria-label="Add Widget" title="Add Widget" className="button add-widget-button propagate-keydown-event" tabindex="0">
+                        <div className="button-icon bowtie-math-plus"></div>
+                    </div>
                 </div>
-            </div></div>
+            </div>
 
 
 
