44c0130e51c92daf4089ce62eba38b0b1aaf525a
Brian.liu <Brian.liu@landasoft.com>
2017-05-19 09:25:17 +0800

    ignore

---
+++ src/component/Chase/FormPanel.js
@@ -0,0 +1,40 @@
+import React,{Component,PropTypes} from 'react'
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+
+class FormPanel extends Component{
+    constructor(props)
+    {
+        super(props)
+        this.state={
+
+            layout:[]
+
+        }
+    }
+
+   render(){
+
+        return (
+
+            <div className='formBackend'>
+                <div className='toolBar'></div>
+
+
+                <div className='formEditor'></div>
+
+                <div className='fieldBar'></div>
+
+
+            </div>
+
+
+        );
+    }
+
+
+}
+
+
+export default DragDropContext(HTML5Backend)(FormPanel);
